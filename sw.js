7df6be7f75e9a711782061a348539bde2cd5089d
Nova <ahmed3987kamel@gmail.com>
2026-08-28 19:46:59 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'nova-tech-v1';
+const assets = [
+  './index.html',
+  './logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
